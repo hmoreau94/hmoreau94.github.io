bdbf2562eba3a8d03e209d51b2f30e0e9e20bad1
hmoreau94 <moreauhugo@me.com>
2016-03-06 16:07:34 +0800

    Added ressources

---
+++ Backup Older Versions/Backup-OnePageScrollWorking/js/custom.js
@@ -0,0 +1,21 @@
+
+// Smooth scrolling helper
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+//Close the navbar when in collapsed mode once clicked.
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
